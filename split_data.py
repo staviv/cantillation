56807cc0aa64a175e3a322cad65487e0a40add38
aviv <88351363+staviv@users.noreply.github.com>
2024-03-03 19:02:41 +0000

    Update .gitignore and modify split_data.py Implement a telegram-bot to easily test the model.

--- split_data.py
+++ split_data.py
@@ -1,31 +1,30 @@
 import json
-
+import random
 def split_data(file_path, train_ratio=0.8):
     # Load the original data
     with open(file_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
-
-    # Initialize the train and validation dictionaries
-    train_data = {"text": [], "ashkenazi": [], "maroko": [], "yerushalmi": [], "bavly": []}
-    validation_data = {"text": [], "ashkenazi": [], "maroko": [], "yerushalmi": [], "bavly": []}
-
-    nusachim = ["ashkenazi", "maroko", "yerushalmi", "bavly"]
-    for nusach in nusachim:
-        # Calculate the split index
-        split_index = int(len(data[nusach]) * train_ratio)
-
-        # Split the data
-        train_data[nusach] = data[nusach][:split_index]
-        validation_data[nusach] = data[nusach][split_index:]
-
-    # Split the text data
-    split_index = int(len(data['text']) * train_ratio)
-    train_data['text'] = data['text'][:split_index]
-    validation_data['text'] = data['text'][split_index:]
-
+    
+    
+    
+    
+    # Split the data
+    random.seed(42)
+    # we want to get use the data as a parts of 50 samples each. so close samples will be close after the split
+    num_samples = len(data['text']) // 50 
+    # get the index of the split including all the 50 samples of the index
+    random_vector = random.sample(range(num_samples), int(num_samples * train_ratio))
+    random_vector.sort()
+    # add all the indexes of the samples
+    random_vector = [i * 50 for i in random_vector]
+    random_vector = [i + j for i in random_vector for j in range(50)]
+    train_data = {key: [data[key][i] for i in random_vector] for key in data.keys()}
+    validation_data = {key: [data[key][i] for i in range(len(data['text'])) if i not in random_vector] for key in data.keys()}
+    
     # Save the train and validation data
     with open('train_data.json', 'w', encoding='utf-8') as f:
         json.dump(train_data, f, ensure_ascii=False, indent=4)
     with open('validation_data.json', 'w', encoding='utf-8') as f:
         json.dump(validation_data, f, ensure_ascii=False, indent=4)
 
+split_data('03_dataset.json', 0.95)
